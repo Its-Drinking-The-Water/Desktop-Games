3b94df15b2fed33a007d82063fb7f87342246530
Its-Drinking-The-Water <ct5236199@gmail.com>
2026-03-09 08:51:32 -0500

    Update Tictactoe.js
    Adds tic tac toe just as the first game, as it had the least pieces. All other games will basically follow this format, just with more pieces

--- Tictactoe.js
+++ Tictactoe.js
@@ -1,1 +1,18 @@
-//This doesn't seem very useful
+//Desktop path
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var shell = new ActiveXObject("WScript.Shell");
+var desktopPath = shell.SpecialFolders("Desktop");
+
+//filenames
+var filenames = ["O", "O", "O", "O", "O", "X", "X", "X", "X", "X"];
+
+//loop
+for (var i = 0; i < filenames.length; i++) {
+    var filePath = desktopPath + "\\" + filenames[i];
+    
+       //make the files
+        var file = fso.CreateTextFile(filePath, true);
+        file.WriteLine("This is a tic-tac-toe piece representing a " + filenames[i]); //text in file
+        file.Close();
+   
+}
