28bde5ee1f5d21dc6e5484f3198b02c643715928
Its-Drinking-The-Water <ct5236199@gmail.com>
2026-03-09 09:32:50 -0500

    Update Checkers.js
    Added checkers, just did them as X and O.

--- Checkers.js
+++ Checkers.js
@@ -1,1 +1,18 @@
+//Desktop path
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var shell = new ActiveXObject("WScript.Shell");
+var desktopPath = shell.SpecialFolders("Desktop");
 
+//filenames
+var filenames = ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "X", "X", "X", "X", "X" , "X", "X", "X", "X", "X" , "X", "X"];
+
+//loop
+for (var i = 0; i < filenames.length; i++) {
+    var filePath = desktopPath + "\\" + filenames[i];
+    
+       //make the files
+        var file = fso.CreateTextFile(filePath, true);
+        file.WriteLine("This is a checker for " + filenames[i]); //text in file
+        file.Close();
+   
+}
